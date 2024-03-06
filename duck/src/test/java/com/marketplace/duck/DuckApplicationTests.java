515056093db83d40d147f18b5297e92ea2f92b3c
Лев Шинкоренко <Lev Shinkorenko>
2024-03-06 17:08:29 +0300

    First commit, add spring files in directory DUCK

---
+++ duck/src/test/java/com/marketplace/duck/DuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketplace.duck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
